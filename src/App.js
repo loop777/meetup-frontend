83ce068a6668205d2154cec9163b3cae2f309ba6
Ricardo Alcântara <ricardo_alcantara2@hotmail.com>
2019-08-18 22:05:01 -0300

    Redux, redux-saga and reactotron setup and configs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import './config/ReactotronConfig';
+
+import history from './services/history';
 import Routes from './routes';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
